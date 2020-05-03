706aa88b226887b5134a71527d52c07126913451
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-05-03 16:06:59 -0500

    build(config): add a podman compatible config
    BREAKING CHANGE:

--- scripts/entrypoint.dev.sh
+++ scripts/entrypoint.dev.sh
@@ -4,8 +4,6 @@ set -e
 
 go run cmd/dbmigrate/main.go
 
-go run cmd/dbmigrate/main.go -dbname=boilerplatetest
-
 GO111MODULE=off go get github.com/githubnemo/CompileDaemon
 
 CompileDaemon --build="go build -o main cmd/api/main.go" --command=./main
